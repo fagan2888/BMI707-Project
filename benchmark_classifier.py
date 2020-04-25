f9dbb881d697d87f336580bc5744eade1a487fd5
mwestt <matt@DESKTOP-ALDFEPK.localdomain>
2020-04-25 02:32:50 -0400

    add data augmentation

--- benchmark_classifier.py
+++ benchmark_classifier.py
@@ -4,11 +4,11 @@
 
 
 import datetime
-from load_data import load_data
-from load_data import load_metadata
+from load_data import load_data, load_metadata, data_generator_from_dataframe, ValidImageDataGenerator
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Flatten, Activation
 from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
 
 
 def create_model():
@@ -92,14 +92,77 @@ def train_benchmark(model, X_train, X_val, y_train, y_val, epochs=12,
     return model
 
 
+def train_augmented_benchmark(model, train_generator, validation_generator,
+                              epochs=12, steps_per_epoch=6, validation_steps=10,
+                              save=False):
+    """A function to train a benchmark CNN in Keras with augmented data.
+    
+    Parameters
+    ----------
+    model : Keras Sequential model
+        Model created using the `create_model` function.
+
+    train_generator : Keras ImageDataGenerator object
+        Object to generate batches of augmented tensor image data for training.
+
+    validation_generator : Keras ImageDataGenerator object
+        Object to generate batches of augmented tensor image data for validation.
+
+    epochs : int
+        Number of training epochs.
+    
+    steps_per_epoch : int
+        Number of steps per epoch.
+
+    validation_steps : int
+        Number of validation steps.
+
+    save : boolean
+        Whether or not to save model.
+    
+    Returns
+    -------
+    model : Keras Sequential Model
+        A trained Keras model. 
+    """    
+    # Compile model
+    model.compile(loss='binary_crossentropy', optimizer='adam', 
+                  metrics=['accuracy'])
+
+    # Train model
+    model.fit_generator(train_generator, validation_data=validation_generator, 
+                        epochs=epochs, steps_per_epoch=steps_per_epoch, 
+                        validation_steps=validation_steps)
+
+    # Save model with timestamp as name
+    if save:
+        now = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        filepath = 'model_aug_bench_' + now + '.h5'
+        model.save(filepath)
+
+    return model
+
+
 if __name__ == '__main__':
     df_train, df_val = load_metadata()
-    images_train, labels_train = load_data(df_train)
-    images_val, labels_val = load_data(df_val)
+    # images_train, labels_train = load_data(df_train)
+    # images_val, labels_val = load_data(df_val)
+
+    # model = create_model()
+    # train_benchmark(model, images_train, images_val, labels_train, labels_val, 
+    #                 epochs=12, batch_size=32, save=False)
+
+    train_datagen = ImageDataGenerator(rescale=1./255, horizontal_flip=True,
+                                         width_shift_range=1)
+
+    train_generator = data_generator_from_dataframe(train_datagen, df_train)
+
+    validation_generator = data_generator_from_dataframe(ValidImageDataGenerator(), df_val)
 
     model = create_model()
-    train_benchmark(model, images_train, images_val, labels_train, labels_val, 
-                    epochs=12, batch_size=32, save=False)
+    train_augmented_benchmark(model, train_generator, validation_generator,
+                              epochs=12, steps_per_epoch=12,
+                              save=False)
 
     # Load saved model
     # model = load_model('model_bench_2020_04_22_22_58_23.h5')    
--- load_data.py
+++ load_data.py
@@ -5,6 +5,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 from PIL import Image
 
@@ -77,18 +78,102 @@ def load_data(df, image_size=(256,256)):
         # Convert to array, normalize and append
         im = np.array(im)
         images.append(im / 255)
-        
+
         label = 1 if (row['finding'] == 'COVID-19' or row['finding'] == 'COVID-19, ARDS') else 0
         labels.append(label)
         
     return np.array(images), np.array(labels)
 
 
-def data_augment():
-    pass
+def TrainImageDataGenerator(shear_range=0.2, zoom_range=0.2, 
+                            horizontal_flip=True):
+    """A wrapper for generating ImageDataGenerator object for training data.
+    
+    Parameters
+    ----------
+    shear_range : float
+        Shear Intensity (Shear angle in counter-clockwise direction in degrees).
+    
+    zoom_range : float
+        Float or [lower, upper]. Range for random zoom.
+
+    horizontal_flip : boolean
+        Boolean. Randomly flip inputs horizontally.
+
+    Returns
+    -------
+    train_generator : Keras ImageDataGenerator object
+        A Keras ImageDataGenerator object with the specified arguments.
+    """
+    train_generator = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=shear_range,
+        zoom_range=zoom_range,
+        horizontal_flip=horizontal_flip)
+
+    return train_generator
+
+
+def ValidImageDataGenerator():
+    """A wrapper for generating ImageDataGenerator object for validation data."""
+    return ImageDataGenerator(rescale=1./255)
+
+
+def data_generator_from_dataframe(datagen, df, image_size=(256, 256), batch_size=32):
+    """A function to process metadata dataframes and return generators for
+    generating augmented training and validation data.
+    
+    Parameters
+    ----------
+    datagen : Keras ImageDataGenerator
+        Keras ImageDataGenerator object
+
+    df : pandas DataFrame
+        Dataframe of metadata.
+
+    image_size : tuple of int
+        Size of images to scale to.
+
+    batch_size : int
+        Batch size to use when fitting.
+
+    Returns
+    -------
+    generator : Keras ImageDataGenerator object
+        Keras ImageDataGenerator object with a defined flow from dataframe.
+    """
+    # Define label 1 for COVID, 0 otherwise
+    df['label'] = ((df['finding'] == 'COVID-19') | (df['finding'] == 'COVID-19, ARDS')).astype('int')
+    
+    # Define data generators
+    generator = datagen.flow_from_dataframe(
+        dataframe=df,
+        directory='images/',
+        x_col="filename",
+        y_col="label",
+        target_size=image_size,
+        batch_size=batch_size,
+        class_mode='raw')
+
+    return generator
 
 
 if __name__ == '__main__':
     df_train, df_val = load_metadata()
-    images_train, labels_train = load_data(df_train)
-    images_val, labels_val = load_data(df_val)
+
+    # images_train, labels_train = load_data(df_train)
+    # images_val, labels_val = load_data(df_val)
+
+    train_generator = ImageDataGenerator(rescale=1./255, horizontal_flip=True,
+                                         width_shift_range=100)
+
+    train_generator = data_generator_from_dataframe(train_generator, df_train)
+
+    validation_generator = data_generator_from_dataframe(ValidImageDataGenerator(), df_val)
+
+    # Generate augmented images
+    for image_batch, label_batch in train_generator:
+        for img, label in zip(image_batch, label_batch):
+            print('Label =', label)
+            plt.imshow(img)
+            plt.show()
